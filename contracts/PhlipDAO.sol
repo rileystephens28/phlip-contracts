84a799f70f99c7f13e7654191d5f3a992d46684a
Riley Stephens <riley.stephens28@gmail.com>
2022-03-04 15:55:37 -0600

    Implement role-based access control

--- contracts/PhlipDAO.sol
+++ contracts/PhlipDAO.sol
@@ -4,23 +4,27 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 /**
  * @title PhlipDAO
  * @author Riley Stephens
- * @notice This is an ownable ERC20 token that implements features for external governance.
+ * @notice This is an ERC20 token will role-base access control that implements features for external governance.
  */
 contract PhlipDAO is
     ERC20,
     ERC20Burnable,
     Pausable,
-    Ownable,
+    AccessControl,
     ERC20Permit,
     ERC20Votes
 {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
     /**
      * @notice Create contract with initial supply of 5,479,500,000 tokens
      */
@@ -29,12 +33,53 @@ contract PhlipDAO is
     }
 
     /**
-     * @notice Allow contract owner to mint tokens to a given address
+     * @notice Allow address with PAUSER role to pause token transfers
+     */
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    /**
+     * @notice Allow address with PAUSER role to unpause token transfers
+     */
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    /**
+     * @notice Allow address with MINTER role to mint tokens to a given address
      * @param to The address to mint tokens to
      * @param amount The amount of tokens to mint
      */
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+    }
+
+    /**
+     * @notice Allow address with MINTER role to mint tokens to a given address
+     * @param account The address holding tokens to burn
+     * @param amount The amount of tokens to burn
+     */
+    function burn(address account, uint256 amount)
+        public
+        onlyRole(BURNER_ROLE)
+    {
+        _burn(account, amount);
+    }
+
+    /**
+     * @notice Function called before tokens are transferred
+     * @dev Override to make sure that token tranfers have not been paused
+     * @param from The address tokens will be transferred from
+     * @param to The address tokens will be transferred  to
+     * @param amount The amount of tokens to transfer
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
     }
 
     /**
