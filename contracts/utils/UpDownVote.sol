551d497e28293f73df219b83017be93c04db80b7
Riley Stephens <riley.stephens28@gmail.com>
2022-03-05 16:47:36 -0600

    Fix scopes and add view functions

--- contracts/utils/UpDownVote.sol
+++ contracts/utils/UpDownVote.sol
@@ -2,51 +2,80 @@
 pragma solidity ^0.8.11;
 
 contract UpDownVote {
-    struct Votes {
-        uint64 upVoteCount;
-        uint64 downVoteCount;
+    /**
+     * A Ballot tracks the number of up and down votes and how each
+     * address voted. Tracking addresses prevents double voting.
+     */
+    struct Ballot {
+        uint256 upVoteCount;
+        uint256 downVoteCount;
         mapping(address => bool) upVoters;
         mapping(address => bool) downVoters;
     }
 
-    mapping(uint256 => Votes) public votes;
+    mapping(uint256 => Ballot) internal _ballots;
 
     /**
-     * @notice Ensure msg.sender has not voted on token
-     * @dev Reverts if msg.send has already voted on token
+     * @notice Returns the number of up votes for a given ballot.
+     * @param _ballotID The ID of a ballot
      */
-    modifier onlyNewVoters(uint256 tokenID) {
-        Votes storage tokenVote = votes[tokenID];
-        require(
-            !tokenVote.upVoters[msg.sender],
-            "UpDownVote: ACCOUNT_ALREADY_UPVOTED"
-        );
-        require(
-            !tokenVote.downVoters[msg.sender],
-            "UpDownVotes: ACCOUNT_ALREADY_DOWNVOTED"
-        );
-        _;
+    function upVotesFor(uint256 _ballotID)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        return _ballots[_ballotID].upVoteCount;
+    }
+
+    /**
+     * @notice Returns the number of down votes for a given ballot.
+     * @param _ballotID The ID of a ballot
+     */
+    function downVotesFor(uint256 _ballotID)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        return _ballots[_ballotID].downVoteCount;
     }
 
     /**
      * @notice Count msg.sender's upvote for a token
-     * @dev Only allows one vote per account per token
+     * @dev Reverts if msg.send has already voted on token
      * @param tokenID The ID of the token to upvote
      */
-    function upVote(uint256 tokenID) public onlyNewVoters(tokenID) {
-        Votes storage tokenVote = votes[tokenID];
-        tokenVote.upVoters[msg.sender] = true;
-        tokenVote.upVoteCount = tokenVote.upVoteCount + 1;
+    function _castUpVote(uint256 tokenID) internal virtual {
+        Ballot storage bal = _ballots[tokenID];
+        require(
+            !bal.upVoters[msg.sender],
+            "UpDownVote: ACCOUNT_ALREADY_UPVOTED"
+        );
+        require(
+            !bal.downVoters[msg.sender],
+            "UpDownVotes: ACCOUNT_ALREADY_DOWNVOTED"
+        );
+        bal.upVoters[msg.sender] = true;
+        bal.upVoteCount = bal.upVoteCount + 1;
     }
 
     /**
      * @notice Count msg.sender's downvote for a token
-     * @dev Only allows one vote per account per token
+     * @dev Reverts if msg.send has already voted on token
      * @param tokenID The ID of the token to downvote
      */
-    function downVote(uint256 tokenID) public onlyNewVoters(tokenID) {
-        Votes storage tokenVote = votes[tokenID];
-        tokenVote.downVoters[msg.sender] = true;
-        tokenVote.downVoteCount = tokenVote.downVoteCount + 1;
+    function _castDownVote(uint256 tokenID) internal virtual {
+        Ballot storage bal = _ballots[tokenID];
+        require(
+            !bal.upVoters[msg.sender],
+            "UpDownVote: ACCOUNT_ALREADY_UPVOTED"
+        );
+        require(
+            !bal.downVoters[msg.sender],
+            "UpDownVotes: ACCOUNT_ALREADY_DOWNVOTED"
+        );
+        bal.downVoters[msg.sender] = true;
+        bal.downVoteCount = bal.downVoteCount + 1;
     }
 }
