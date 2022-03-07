a88420c3903e50461100ae1bf8da74afa373e501
Riley Stephens <riley.stephens28@gmail.com>
2022-03-07 10:43:49 -0600

    Add in constructor params

--- contracts/tokens/PinkCard.sol
+++ contracts/tokens/PinkCard.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.11;
 
 import "./PhlipCard.sol";
+import "./IPhlipCard.sol";
 
-contract PinkCard is PhlipCard {
-    constructor() PhlipCard("Pink Phlip Card", "PPC") {}
+contract PinkCard is PhlipCard, IPhlipCard {
+    constructor(
+        string memory _baseUri,
+        uint256 _maxDownvotes,
+        uint256 _maxUriChanges,
+        uint256 _minDaoTokensRequired,
+        address _daoTokenAddress
+    )
+        PhlipCard(
+            "Phlip Pink Card",
+            "PPC",
+            _baseUri,
+            _maxDownvotes,
+            _maxUriChanges,
+            _minDaoTokensRequired,
+            _daoTokenAddress
+        )
+    {}
 }
