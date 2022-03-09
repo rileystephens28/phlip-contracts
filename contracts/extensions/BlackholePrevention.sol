7345506eb99ccc85bf8028f086fb0401787563ec
Riley Stephens <riley.stephens28@gmail.com>
2022-03-09 12:54:08 -0600

    Add blackhole prevention contract

---
+++ contracts/extensions/BlackholePrevention.sol
@@ -0,0 +1,76 @@
+pragma solidity >=0.8.11;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+/**
+ * @notice Prevents ETH or Tokens from getting stuck in a contract by allowing
+ *  the Owner/DAO to pull them out on behalf of a user
+ * This is only meant to contracts that are not expected to hold tokens, but do handle transferring them.
+ */
+contract BlackholePrevention {
+    using Address for address payable;
+    using SafeERC20 for IERC20;
+
+    event WithdrawStuckEther(address indexed receiver, uint256 amount);
+    event WithdrawStuckERC20(
+        address indexed receiver,
+        address indexed tokenAddress,
+        uint256 amount
+    );
+    event WithdrawStuckERC721(
+        address indexed receiver,
+        address indexed tokenAddress,
+        uint256 indexed tokenId
+    );
+
+    function _withdrawEther(address payable receiver, uint256 amount)
+        internal
+        virtual
+    {
+        require(
+            receiver != address(0x0),
+            "Reciever cannot be the zero address"
+        );
+        if (address(this).balance >= amount) {
+            receiver.sendValue(amount);
+            emit WithdrawStuckEther(receiver, amount);
+        }
+    }
+
+    function _withdrawERC20(
+        address payable receiver,
+        address tokenAddress,
+        uint256 amount
+    ) internal virtual {
+        require(
+            receiver != address(0x0),
+            "Reciever cannot be the zero address"
+        );
+        if (IERC20(tokenAddress).balanceOf(address(this)) >= amount) {
+            IERC20(tokenAddress).safeTransfer(receiver, amount);
+            emit WithdrawStuckERC20(receiver, tokenAddress, amount);
+        }
+    }
+
+    function _withdrawERC721(
+        address payable receiver,
+        address tokenAddress,
+        uint256 tokenId
+    ) internal virtual {
+        require(
+            receiver != address(0x0),
+            "Reciever cannot be the zero address"
+        );
+        if (IERC721(tokenAddress).ownerOf(tokenId) == address(this)) {
+            IERC721(tokenAddress).transferFrom(
+                address(this),
+                receiver,
+                tokenId
+            );
+            emit WithdrawStuckERC721(receiver, tokenAddress, tokenId);
+        }
+    }
+}
