da513ef51090e7fc9fd8639a8a7c48cb229d2123
Riley Stephens <riley.stephens28@gmail.com>
2022-04-06 18:11:09 -0500

    Create mock for game record testing

---
+++ contracts/mocks/extensions/GameRecordMock.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "../../extensions/GameRecord.sol";
+
+contract GameRecordMock is GameRecord {
+    bool public didProtectedAction;
+
+    constructor() {
+        didProtectedAction = false;
+    }
+
+    function createGameRecord(uint256 _id) public {
+        _createGameRecord(_id);
+    }
+
+    function recordNoRewardWin(uint256 _id) public {
+        _recordWin(_id);
+    }
+
+    function recordTokenWin(
+        uint256 _id,
+        address _token,
+        uint256 _amount
+    ) public {
+        _recordTokenWin(_id, _token, _amount);
+    }
+
+    function recordEthWin(uint256 _id, uint256 _amount) public {
+        _recordEthWin(_id, _amount);
+    }
+
+    function recordLoss(uint256 _id) public {
+        _recordLoss(_id);
+    }
+}
