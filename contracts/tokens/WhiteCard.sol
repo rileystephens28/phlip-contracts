9838fa5775956f9fcdd9d3a0fd815d9f5643f256
Riley Stephens <riley.stephens28@gmail.com>
2022-04-06 19:54:06 -0500

    Implement GameRecord ext

--- contracts/tokens/WhiteCard.sol
+++ contracts/tokens/WhiteCard.sol
@@ -2,21 +2,15 @@
 pragma solidity 0.8.11;
 
 import "./PhlipCard.sol";
+import "../extensions/GameRecord.sol";
 
 /**
  * @title WhiteCard
  * @author Riley Stephens
  * @dev Implementation of a PhlipCard with with basic game recording functionality.
  */
-contract WhiteCard is PhlipCard {
+contract WhiteCard is PhlipCard, GameRecord {
     bytes32 public constant RECORDER_ROLE = keccak256("RECORDER_ROLE");
-
-    struct GameRecord {
-        uint256 wins;
-        uint256 losses;
-    }
-
-    mapping(uint256 => GameRecord) private _gameRecords;
 
     constructor(
         string memory _baseUri,
@@ -39,30 +33,65 @@ contract WhiteCard is PhlipCard {
     }
 
     /**
-     * @dev Increment the number of wins by 1 for a given card.
-     * @param _tokenID The ID of the token to record.
+     * @dev Overrides PhlipCard._mintCard() to include game recording functionality.
+     * @param _cardID The ID of card being minted
+     * @param _to The address to mint card to
+     * @param _uri The IPFS CID referencing the new card's metadata
      */
-    function recordWin(uint256 _tokenID)
-        external
-        tokenExists(_tokenID)
-        onlyRole(RECORDER_ROLE)
-    {
-        // NOTE - Should we explicitly prevent recording wins on unplayable cards?
-        GameRecord storage game = _gameRecords[_tokenID];
-        game.wins += 1;
+    function _mintCard(
+        uint256 _cardID,
+        address _to,
+        string memory _uri
+    ) internal override {
+        super._mintCard(_cardID, _to, _uri);
+
+        // Create a ballot for the new card
+        _createGameRecord(_cardID);
     }
 
     /**
-     * @dev Increment the number of losses by 1 for a given card.
-     * @param _tokenID The ID of the token to record.
+     * @dev Records a game win (NO REWARDS)
+     * @param _cardID The ID of the card to record.
      */
-    function recordLoss(uint256 _tokenID)
+    function recordWin(uint256 _cardID) external onlyRole(RECORDER_ROLE) {
+        // NOTE - Should we explicitly prevent recording wins on unplayable cards?
+        _recordWin(_cardID);
+    }
+
+    /**
+     * @dev Records a game win (ETH REWARDS)
+     * @param _cardID The ID of the card to record.
+     * @param _amount The amount of ETH won.
+     */
+    function recordWin(uint256 _cardID, uint256 _amount)
         external
-        tokenExists(_tokenID)
         onlyRole(RECORDER_ROLE)
     {
+        // NOTE - Should we explicitly prevent recording wins on unplayable cards?
+        _recordEthWin(_cardID, _amount);
+    }
+
+    /**
+     * @dev Records a game win (TOKEN REWARDS)
+     * @param _cardID The ID of the card to record.
+     * @param _token The address of token rewarded for winning.
+     * @param _amount The amount of tokens won.
+     */
+    function recordWin(
+        uint256 _cardID,
+        address _token,
+        uint256 _amount
+    ) external onlyRole(RECORDER_ROLE) {
+        // NOTE - Should we explicitly prevent recording wins on unplayable cards?
+        _recordTokenWin(_cardID, _token, _amount);
+    }
+
+    /**
+     * @dev Records a game loss for a given card
+     * @param _cardID The ID of the card to record.
+     */
+    function recordLoss(uint256 _cardID) external onlyRole(RECORDER_ROLE) {
         // NOTE - Should we explicitly prevent recording losses on unplayable cards?
-        GameRecord storage game = _gameRecords[_tokenID];
-        game.losses += 1;
+        _recordLoss(_cardID);
     }
 }
