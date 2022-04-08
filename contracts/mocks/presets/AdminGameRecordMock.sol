4d90bcb233b4aec47344b66e8cf1035b003e5200
Riley Stephens <riley.stephens28@gmail.com>
2022-04-08 11:16:38 -0500

    Create AdminGameRecord mock contract

---
+++ contracts/mocks/presets/AdminGameRecordMock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "../../presets/AdminGameRecord.sol";
+
+/**
+ * @title AdminGameRecordMock
+ * @author Riley Stephens
+ * @dev Mock contract for AdminGameRecorder.
+ */
+contract AdminGameRecordMock is AdminGameRecord {
+    constructor() AdminGameRecord() {}
+
+    /**
+     * @dev External function to create a game record
+     * @param _newRecordID The ID of new record to create
+     */
+    function createGameRecord(uint256 _newRecordID)
+        external
+        onlyRole(RECORDER_ROLE)
+    {
+        // Create a game record for the new card
+        _createGameRecord(_newRecordID);
+    }
+}
