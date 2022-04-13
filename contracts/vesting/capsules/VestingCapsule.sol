466510c9e8730e41ab72af8de362b589c9eb13cf
Riley Stephens <riley.stephens28@gmail.com>
2022-04-13 18:26:44 -0500

    Separate into capsule and vault dirs

--- contracts/vesting/capsules/VestingCapsule.sol
+++ contracts/vesting/capsules/VestingCapsule.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./VestingVault.sol";
+import "../vaults/VestingVault.sol";
 
 /**
  * @title VestingCapsule
