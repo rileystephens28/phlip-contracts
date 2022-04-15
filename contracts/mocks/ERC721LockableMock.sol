a7c352b9503d29d3a33cb8a592beb8a42f6816d9
Riley Stephens <riley.stephens28@gmail.com>
2022-04-14 19:52:07 -0500

    Add external mint function

--- contracts/mocks/ERC721LockableMock.sol
+++ contracts/mocks/ERC721LockableMock.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
-
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "../extensions/ERC721Lockable.sol";
 
-contract ERC721LockableMock is ERC721Lockable {
+contract ERC721LockableMock is ERC721Lockable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public tokenIds;
+
     constructor() ERC721("ERC721LockableMock", "LOCK") {}
+
+    function mint(address to) public onlyOwner {
+        uint256 id = tokenIds.current();
+        tokenIds.increment();
+        _mint(to, id);
+    }
 }
