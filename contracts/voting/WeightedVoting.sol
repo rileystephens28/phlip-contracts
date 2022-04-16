951871098efc1053b6cbf72521ffc5d1985750e9
Riley Stephens <riley.stephens28@gmail.com>
2022-04-15 21:35:38 -0500

    Implement weighted voting system

---
+++ contracts/voting/WeightedVoting.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+/**
+ * @title WeightedBallot
+ * @author Riley Stephens
+ * @dev Provides contract with the ability to create and manage a weighted
+ * voting system. The system allows for a single up or down vote to be cast per
+ * address per ballot. In order to prevent spam voting, directinal voting
+ * frequency reduces the impact of each subsequent vote in that respective direction.
+ * This means that if an address up votes more than they down vote, then the down
+ * votes of that address will carry more weight than the up votes.
+ *
+ * PRECISION = 10000 (to represent 2 decimal places)
+ * Up vote weight is calculated as:
+ *  PRECISION - (Num up votes cast * PRECISION / total votes cast)
+ *
+ * Down vote weight is calculated as:
+ *  PRECISION - (Num down votes cast * PRECISION / total votes cast)
+ *
+ *
+ * Example Scenario: Address up votes 10 times and down votes 5 times.
+ * - Up vote weight: 10000 - (10 * 10000 / 15) = 3334 => .3334 or 33.34%
+ * - Down vote weight: 10000 - (5 * 10000 / 15) = 6666 => .6666 or 66.66%
+ *
+ */
+contract WeightedBallot {
+    uint256 private PRECISION = 10000;
+
+    /**
+     * @dev A Voter describes and address by storing
+     * the number of upand down votes they have cast
+     * @param upCount The number of up votes cast.
+     * @param downCount The number of up votes cast.
+     * @param voteCount The total number of votes cast.
+     */
+    struct Voter {
+        uint256 upCount;
+        uint256 downCount;
+        uint256 voteCount;
+        uint256 upWeight;
+        uint256 downWeight;
+    }
+
+    /**
+     * @dev A Ballot tracks up/down vote values
+     * and voting addresses to prevent double voting.
+     * @param upVoteVal Sum of up vote weights
+     * @param downvoteCount Sum of down vote weights
+     * @param upVoters Mapping of addresses that up voted.
+     * @param downVoters Mapping of addresses that down voted.
+     * @param voteWeights Mapping weight of addresses casted vote.
+     */
+    struct Ballot {
+        uint256 upVoteVal;
+        uint256 downVoteVal;
+        mapping(address => bool) upVoters;
+        mapping(address => bool) downVoters;
+        mapping(address => uint256) voteWeights;
+    }
+
+    mapping(address => Voter) internal _voters;
+
+    mapping(uint256 => Ballot) internal _ballots;
+
+    /***********************************|
+    |          View Functions           |
+    |__________________________________*/
+
+    /**
+     * @dev Accessor function for getting a ballots upVoteVal.
+     * @param _ballotID The ID of a ballot
+     * @return The number of up votes cast on a given ballot.
+     */
+    function upVoteValueOf(uint256 _ballotID)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        return _ballots[_ballotID].upVoteVal;
+    }
+
+    /**
+     * @dev Accessor function for getting a ballots downVoteVal.
+     * @param _ballotID The ID of a ballot
+     * @return The number of down votes cast on a given ballot.
+     */
+    function downVoteValueOf(uint256 _ballotID)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        return _ballots[_ballotID].downVoteVal;
+    }
+
+    /***********************************|
+    |        Private Functions          |
+    |__________________________________*/
+
+    /**
+     * @dev Records up vote by increasing ballots up vote value by
+     * the voters up vote weight. Undo previous down vote if one exists
+     * @param _ballotID The ID of the ballot to up vote.
+     * @param _voter Address of the voter casting the vote.
+     */
+    function _castUpVote(uint256 _ballotID, address _voter) internal virtual {
+        Ballot storage ballot = _ballots[_ballotID];
+        require(
+            !ballot.upVoters[msg.sender],
+            "WeightedVoting: Already casted up vote"
+        );
+        Voter storage voter = _voters[_voter];
+
+        // If voter has already cast a down vote for this
+        // ballot, delete previous down vote record
+        if (ballot.downVoters[_voter]) {
+            delete ballot.downVoters[_voter];
+            ballot.downVoteVal -= ballot.voteWeights[_voter];
+            voter.downCount -= 1;
+        }
+
+        // Update voters up vote weight
+        uint256 upVoteWeight = _calcVoterWeight(
+            voter.upCount,
+            voter.voteCount,
+            PRECISION
+        );
+        voter.upWeight = upVoteWeight;
+
+        // Record up vote
+        ballot.upVoters[_voter] = true;
+        ballot.upVoteVal += upVoteWeight;
+    }
+
+    /**
+     * @dev Records down vote by increasing ballots down vote value by
+     * the voters down vote weight. Undo previous up vote if one exists
+     * @param _ballotID The ID of the ballot to up vote.
+     * @param _voter Address of the voter casting the vote.
+     */
+    function _castDownVote(uint256 _ballotID, address _voter) internal virtual {
+        Ballot storage ballot = _ballots[_ballotID];
+        require(
+            !ballot.downVoters[msg.sender],
+            "WeightedVoting: Already casted down vote"
+        );
+        Voter storage voter = _voters[_voter];
+
+        // If voter has already cast an up vote for this
+        // ballot, delete previous up vote record
+        if (ballot.upVoters[_voter]) {
+            delete ballot.upVoters[_voter];
+            ballot.upVoteVal -= ballot.voteWeights[_voter];
+            voter.upCount -= 1;
+        }
+
+        // Update voters up vote weight
+        uint256 downVoteWeight = _calcVoterWeight(
+            voter.downCount,
+            voter.voteCount,
+            PRECISION
+        );
+        voter.downWeight = downVoteWeight;
+
+        // Record down vote
+        ballot.downVoters[_voter] = true;
+        ballot.downVoteVal += downVoteWeight;
+    }
+
+    /***********************************|
+    |          Pure Functions           |
+    |__________________________________*/
+
+    /**
+     * @dev Calculate the value of a voter for either direction.
+     * @param _part The number of up or down votes cast.
+     * @param _whole The total number of votes cast.
+     * @param _precision The precision of the vote (default is 10000).
+     * @return The weighted value of the vote.
+     */
+    function _calcVoterWeight(
+        uint256 _part,
+        uint256 _whole,
+        uint256 _precision
+    ) internal pure virtual returns (uint256) {
+        return _precision - ((_part * _precision) / _whole);
+    }
+}
