44b07cc9a6c460d4a211ed276efce286e77da1a7
Riley Stephens <riley.stephens28@gmail.com>
2022-04-15 22:08:55 -0500

    Add view functions to IVoucherIssuer

--- contracts/vouchers/IVoucherIssuer.sol
+++ contracts/vouchers/IVoucherIssuer.sol
@@ -11,6 +11,27 @@ pragma solidity 0.8.11;
  */
 interface IVoucherIssuer {
     /**
+     * @dev Accessor function to see if an address has any vouchers.
+     * @param _address The address to check.
+     * @return Whether or not the address has >= 1 vouchers
+     */
+    function hasVoucher(address _address) external view returns (bool);
+
+    /**
+     * @dev Accessor function for address of voucher holder
+     * @param _id ID associated with voucher.
+     * @return Address of voucher holder.
+     */
+    function voucherHolderOf(uint256 _id) external view returns (address);
+
+    /**
+     * @dev Helper to get vouchers of claims for an address.
+     * @param _for Address of check for remaing vouchers.
+     * @return Number of vouchers for an address.
+     */
+    function remainingVouchers(address _for) external view returns (uint256);
+
+    /**
      * @dev Issue a voucher to an address.
      * @param _to The address to check.
      */
