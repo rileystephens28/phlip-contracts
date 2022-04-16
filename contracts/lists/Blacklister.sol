4cb9c0b0a7eeb092de8b6399032223ec21745651
Riley Stephens <riley.stephens28@gmail.com>
2022-04-16 17:14:21 -0500

    Create abstract blacklister

---
+++ contracts/lists/Blacklister.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./Blacklistable.sol";
+
+/**
+ * @title Blacklister
+ * @author Riley Stephens
+ * @dev Contract that provides default blacklist functionality with role based access control.
+ */
+abstract contract Blacklister is AccessControl, Blacklistable {
+    bytes32 public constant BLOCKER_ROLE = keccak256("BLOCKER_ROLE");
+
+    /**
+     * @dev Allow address with BLOCKER role to add an address to the blacklist
+     * @param _address The address to add to the blacklist
+     */
+    function blacklistAddress(address _address)
+        external
+        virtual
+        onlyRole(BLOCKER_ROLE)
+    {
+        _addToBlacklist(_address);
+    }
+
+    /**
+     * @dev Allow address with BLOCKER role to remove an address from the blacklist
+     * @param _address The address to remove from the blacklist
+     */
+    function unblacklistAddress(address _address)
+        external
+        virtual
+        onlyRole(BLOCKER_ROLE)
+    {
+        _removeFromBlacklist(_address);
+    }
+}
