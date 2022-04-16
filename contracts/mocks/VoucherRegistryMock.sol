fbb4df86bc71e67d63d189f4e493affb5d3b2b2e
Riley Stephens <riley.stephens28@gmail.com>
2022-04-16 14:10:16 -0500

    Simplify VoucherRegistry mock contract

---
+++ contracts/mocks/VoucherRegistryMock.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+import "../vouchers/VoucherRegistry.sol";
+
+contract VoucherRegistryMock is VoucherRegistry {
+    bool public didProtectedAction;
+
+    constructor() {
+        didProtectedAction = false;
+    }
+
+    function protectedAction() public onlyVoucherHolders {
+        didProtectedAction = true;
+    }
+
+    function issueVoucher(address _to, uint256 _id) external {
+        _issueVoucher(_to, _id);
+    }
+
+    function redeemVoucher(uint256 _id) external {
+        _redeemVoucher(msg.sender, _id);
+    }
+}
