c039cf3aa6b3d5ca089967addce8c0c7d3d48f05
Riley Stephens <riley.stephens28@gmail.com>
2022-04-29 12:39:25 -0500

    Test creating multi card packs and purchasing

--- test/sale/PhlipSale.test.js
+++ test/sale/PhlipSale.test.js
@@ -149,6 +149,16 @@ contract("PhlipSale", (accounts) => {
         price.should.be.bignumber.equal(new BN(val));
     };
 
+    const verifyCardMaxSupply = async (cardId, val) => {
+        const max = await saleInstance.maxSupplyOf(cardId);
+        max.should.be.bignumber.equal(new BN(val));
+    };
+
+    const verifyCardMintedSupply = async (cardId, val) => {
+        const minted = await saleInstance.mintedSupplyOf(cardId);
+        minted.should.be.bignumber.equal(new BN(val));
+    };
+
     const verifyRemainingForSale = async (pkgId, val) => {
         const remaining = await saleInstance.getPackagesRemaining(pkgId);
         remaining.should.be.bignumber.equal(new BN(val));
@@ -179,19 +189,20 @@ contract("PhlipSale", (accounts) => {
         id,
         cardInstance,
         tokenAddress,
-        rate = baseRate,
+        amount = baseDaoAmount,
         cliff = baseCliff,
-        duration = baseDuration,
-        amount = baseDaoAmount
+        duration = baseDuration
     ) => {
         const schedule = await cardInstance.getSchedule(id);
 
+        duration = new BN(duration);
+
         // check that the new schedule has correct values
         schedule["token"].should.be.equal(tokenAddress);
-        schedule["rate"].should.be.bignumber.equal(rate);
+        schedule["amount"].should.be.bignumber.equal(amount);
+        schedule["rate"].should.be.bignumber.equal(amount.div(duration));
         schedule["cliff"].should.be.bignumber.equal(cliff);
         schedule["duration"].should.be.bignumber.equal(duration);
-        schedule["amount"].should.be.bignumber.equal(amount);
     };
 
     const verifyTotalReserves = async (scheduleId, cardInstance, val) => {
@@ -202,6 +213,26 @@ contract("PhlipSale", (accounts) => {
     const verifyAvailableReserves = async (scheduleId, cardInstance, val) => {
         const reserves = await cardInstance.availableReservesOf(scheduleId);
         reserves.should.be.bignumber.equal(new BN(val));
+    };
+
+    const verifyTokenBalance = async (token, address, amount) => {
+        const tokenBalance = await token.balanceOf(address);
+        tokenBalance.should.be.bignumber.equal(new BN(amount));
+    };
+
+    const verifyCardMinter = async (cardInstance, cardId, address) => {
+        const cardMinter = await cardInstance.minterOf(new BN(cardId));
+        cardMinter.should.be.equal(address);
+    };
+
+    const verifyCardOwner = async (cardInstance, cardId, address) => {
+        const cardOwner = await cardInstance.ownerOf(new BN(cardId));
+        cardOwner.should.be.equal(address);
+    };
+
+    const verifyCardBalance = async (cardInstance, address, amount) => {
+        const cardBalance = await cardInstance.balanceOf(address);
+        cardBalance.should.be.bignumber.equal(new BN(amount));
     };
 
     beforeEach(async () => {
@@ -548,36 +579,267 @@ contract("PhlipSale", (accounts) => {
 
     describe("Creating Multi Card package", async () => {
         // Failure case
+        it("should fail when package ID already exists", async () => {
+            await createMultiCardPackage();
+            await expectRevert(
+                createMultiCardPackage(),
+                "Package already exists"
+            );
+        });
+        it("should fail when package price is 0", async () => {
+            await expectRevert(
+                createMultiCardPackage(0, 0),
+                "Price cannot be 0"
+            );
+        });
+        it("should fail when number of packages for sale is 0", async () => {
+            await expectRevert(
+                createMultiCardPackage(0, tokenUnits(1), 0),
+                "Number of packages cannot be 0"
+            );
+        });
+        it("should fail when number of cards is 0", async () => {
+            await expectRevert(
+                createMultiCardPackage(
+                    0,
+                    tokenUnits(1),
+                    baseSaleQty,
+                    [new BN(0), new BN(1)],
+                    [new BN(1), new BN(0)]
+                ),
+                "Number of cards cannot be 0"
+            );
+        });
+        it("should fail when DAO token amount is 0", async () => {
+            await expectRevert(
+                createMultiCardPackage(
+                    0,
+                    tokenUnits(1),
+                    baseSaleQty,
+                    [new BN(0), new BN(1)],
+                    [new BN(1), new BN(2)],
+                    [tokenUnits(0), tokenUnits(200)]
+                ),
+                "DAO token amount cannot be 0"
+            );
+        });
+        it("should fail when P2E token amount is 0", async () => {
+            await expectRevert(
+                createMultiCardPackage(
+                    0,
+                    tokenUnits(1),
+                    baseSaleQty,
+                    [new BN(0), new BN(1)],
+                    [new BN(1), new BN(2)],
+                    [tokenUnits(100), tokenUnits(200)],
+                    [tokenUnits(0), tokenUnits(200)]
+                ),
+                "P2E token amount cannot be 0"
+            );
+        });
 
         // Passing case
-        it("should pass when params are valid", async () => {
+        it("should pass when package contains pink text & image cards", async () => {
             const receipt = await createMultiCardPackage();
-            // await expectEvent(receipt, "AddToBlacklist", {
-            //     account: account2,
-            // });
-            // await verifyBlacklisted(account2);
+            expectEvent(receipt, "CreatePackage", {
+                id: new BN(0),
+                price: tokenUnits(1),
+                numForSale: baseSaleQty,
+            });
+
+            // Ensure text card vesting schedules were created
+            await validateSchedule(
+                0,
+                pinkCardInstance,
+                daoInstance.address,
+                tokenUnits(100)
+            );
+            await validateSchedule(
+                1,
+                pinkCardInstance,
+                p2eInstance.address,
+                tokenUnits(1000)
+            );
+
+            // Ensure image card vesting schedules were created
+            await validateSchedule(
+                2,
+                pinkCardInstance,
+                daoInstance.address,
+                tokenUnits(200)
+            );
+            await validateSchedule(
+                3,
+                pinkCardInstance,
+                p2eInstance.address,
+                tokenUnits(2000)
+            );
+
+            // Ensure text vesting schedule reserves were filled
+            await verifyTotalReserves(
+                0,
+                pinkCardInstance,
+                tokenUnits(100).mul(new BN(1)).mul(baseSaleQty)
+            );
+            await verifyTotalReserves(
+                1,
+                pinkCardInstance,
+                tokenUnits(1000).mul(new BN(1)).mul(baseSaleQty)
+            );
+
+            // Ensure image vesting schedule reserves were filled
+            await verifyTotalReserves(
+                2,
+                pinkCardInstance,
+                tokenUnits(200).mul(new BN(2)).mul(baseSaleQty)
+            );
+            await verifyTotalReserves(
+                3,
+                pinkCardInstance,
+                tokenUnits(2000).mul(new BN(2)).mul(baseSaleQty)
+            );
+
+            // Ensure package was created
+            await verifyPackagePrice(0, tokenUnits(1));
+            await verifyRemainingForSale(0, baseSaleQty);
+            await validateCardBundle(
+                0,
+                [new BN(0), new BN(1)],
+                [new BN(1), new BN(2)],
+                [
+                    [new BN(0), new BN(1)],
+                    [new BN(2), new BN(3)],
+                ]
+            );
+        });
+        it("should pass when package contains pink & white cards", async () => {
+            const receipt = await createMultiCardPackage(
+                0,
+                tokenUnits(2),
+                baseSaleQty,
+                [new BN(0), new BN(2)]
+            );
+            expectEvent(receipt, "CreatePackage", {
+                id: new BN(0),
+                price: tokenUnits(2),
+                numForSale: baseSaleQty,
+            });
+
+            // Ensure text card vesting schedules were created
+            await validateSchedule(
+                0,
+                pinkCardInstance,
+                daoInstance.address,
+                tokenUnits(100)
+            );
+            await validateSchedule(
+                1,
+                pinkCardInstance,
+                p2eInstance.address,
+                tokenUnits(1000)
+            );
+
+            // Ensure image card vesting schedules were created
+            await validateSchedule(
+                0,
+                whiteCardInstance,
+                daoInstance.address,
+                tokenUnits(200)
+            );
+            await validateSchedule(
+                1,
+                whiteCardInstance,
+                p2eInstance.address,
+                tokenUnits(2000)
+            );
+
+            // Ensure text vesting schedule reserves were filled
+            await verifyTotalReserves(
+                0,
+                pinkCardInstance,
+                tokenUnits(100).mul(new BN(1)).mul(baseSaleQty)
+            );
+            await verifyTotalReserves(
+                1,
+                pinkCardInstance,
+                tokenUnits(1000).mul(new BN(1)).mul(baseSaleQty)
+            );
+
+            // Ensure image vesting schedule reserves were filled
+            await verifyTotalReserves(
+                0,
+                whiteCardInstance,
+                tokenUnits(200).mul(new BN(2)).mul(baseSaleQty)
+            );
+            await verifyTotalReserves(
+                1,
+                whiteCardInstance,
+                tokenUnits(2000).mul(new BN(2)).mul(baseSaleQty)
+            );
+
+            // Ensure package was created
+            await verifyPackagePrice(0, tokenUnits(2));
+            await verifyRemainingForSale(0, baseSaleQty);
+            await validateCardBundle(
+                0,
+                [new BN(0), new BN(2)],
+                [new BN(1), new BN(2)],
+                [
+                    [new BN(0), new BN(1)],
+                    [new BN(0), new BN(1)],
+                ]
+            );
         });
     });
 
-    describe("Purchasing Card package", async () => {
+    describe.only("Purchasing Card package", async () => {
         beforeEach(async () => {
-            await createMultiCardPackage();
+            await createSingleCardPackage();
+            await createMultiCardPackage(1);
+            await setPresaleActive(true);
         });
 
         // Failure case
-        it("should fail when package ID is not valid", async () => {});
-        it("should fail when presale is not active", async () => {});
-        it("should fail when package sold out", async () => {});
-        it("should fail when caller sends less ETH than package price", async () => {});
+        it("should fail when package ID is not valid", async () => {
+            await expectRevert(
+                purchasePackage(1, tokenUnits(1)),
+                "Package does not exist"
+            );
+        });
+        it("should fail when presale is not active", async () => {
+            await setPresaleActive(false);
+            await expectRevert(purchasePackage(), "Presale is not active");
+        });
+        it("should fail when package sold out", async () => {
+            for (let i = 0; i < baseSaleQty; i++) {
+                await purchasePackage();
+            }
+            await expectRevert(purchasePackage(), "Package not available");
+        });
+        it("should fail when caller sends less ETH than package price", async () => {
+            await expectRevert(
+                purchasePackage(0, tokenUnits(0.5)),
+                "Not enough ETH to cover cost"
+            );
+        });
 
         // Passing case
-        it("should pass when params are valid", async () => {
-            await setPresaleActive(true);
+        it.only("should pass when purchasing single card package", async () => {
             const receipt = await purchasePackage();
-            // await expectEvent(receipt, "AddToBlacklist", {
-            //     account: account2,
-            // });
-            // await verifyBlacklisted(account2);
+            expectEvent(receipt, "PurchasePackage", {
+                id: new BN(0),
+                purchaser: account1,
+            });
+
+            // Ensure package was marked as sold
+            await verifyRemainingForSale(0, baseSaleQty - 1);
+            await verifyCardMaxSupply(0, 675);
+            await verifyCardMintedSupply(0, 2);
+
+            // Ensure account was credited with correct number & type of cards
+            await verifyCardBalance(pinkCardInstance, account1, 2);
+            await verifyCardOwner(pinkCardInstance, 0, account1);
+            await verifyCardOwner(pinkCardInstance, 1, account1);
         });
     });
 });
